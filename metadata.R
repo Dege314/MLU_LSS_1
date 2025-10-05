398ac9328623a8175d2f98bb1b42e93e34b8fcf3
Dege314 <demian.genau@hotmail.de>
2025-10-05 22:18:51 +0200

    upload metadata of cloud masking

--- metadata.R
+++ metadata.R
@@ -1,4 +1,4 @@
-packages <- c("xml2", "ggplot2")
+packages <- c("xml2", "ggplot2", "terra", "dplyr", "ggpubr")
 for (pkg in packages) {
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg)  # Paket installieren
@@ -69,3 +69,78 @@ ggplot() +
 str(meta_data)
 str(S2_dates)
 str(ref_data)
+
+# cloud mask counting and plot
+tif_files <- list.files("Data/S2/pro/mosaic", pattern = "\\.tif$", full.names = TRUE)
+plot(rast(tif_files[1]))
+rast(tif_files[1])
+
+cloud_count <- data.frame()
+plot_list <- list()
+for (i in seq_along(tif_files)) {
+  cat(sprintf("\rCounting and plotting - progress: %d of %d (%s)", i, length(tif_files), basename(tif_files[i])))
+  img <- rast(tif_files[i])
+  total_pixels <- ncell(img[[1]])
+  na_pixels <- sum(is.na(values(img[[1]])))
+  non_na_pixels <- total_pixels - na_pixels
+  na_percent <- (na_pixels / total_pixels) * 100
+  date <- format(as.Date(substr(basename(tif_files[i]), 12, 19), format = "%Y%m%d"), "%Y-%m")
+  cloud_data <- data.frame(date = date, total = total_pixels, na = na_pixels, non_na = non_na_pixels, na_percent = na_percent)
+  # create plot
+  image_df <- as.data.frame(img$B8A, xy=TRUE)
+  plot_list[[i]] <- ggplot(image_df, aes(x = x, y = y)) +
+                           geom_raster() +
+                           scale_fill_viridis_c() +
+                           ggtitle(paste0(date, " - Cloud-Mask: ", round(na_percent, 1), "%"))
+  if (i == 1) {
+    cloud_count <- cloud_data
+  } else {
+    cloud_count <- rbind(cloud_count, cloud_data)
+  }
+  if (i == length(tif_files)) {
+    final_plot <- do.call(ggarrange, c(plot_list, list(ncol = 3, nrow = 3)))
+    ggsave("Data/plots/cloud_masks.png", final_plot, width = 15, height = 15)
+    write.csv(cloud_count, "data/tables/S2_cloud_percentage_per_date.csv", row.names = FALSE)
+    cat("\rCounting finished!                                                      \n")
+  }
+}
+View(cloud_count)
+sum(is.na(values(rast(tif_files[[1]]))))
+sum(!is.na(values(rast(tif_files[[1]]))))
+
+combined <- do.call(ggarrange, c(p_list, list(ncol = 4, nrow = ceiling(length(p_list)/4), common.legend = TRUE, legend = "right")))
+
+final_plot <- annotate_figure(combined, top = text_grob("Boxplots of the best distinguishable variables per class", size = 14, face = "bold"))
+
+plot(plot_list[[1]])
+str(plot_list[[1]])
+
+png("Data/plots/cloud_masks.png", width = 15, height = 15, units = "in", res = 300)
+par(mfrow = c(3, 3),
+    mai = c(0.6, 0.6, 0.8, 0.2)) # margins: bottom, left, top, right
+cloud_count <- data.frame()
+plot_list <- list()
+for (i in seq_along(tif_files)) {
+  cat(sprintf("\rCounting and plotting - progress: %d of %d (%s)", i, length(tif_files), basename(tif_files[i])))
+  img <- rast(tif_files[i])
+  total_pixels <- ncell(img[[1]])
+  na_pixels <- sum(is.na(values(img[[1]])))
+  non_na_pixels <- total_pixels - na_pixels
+  na_percent <- (na_pixels / total_pixels) * 100
+  date <- format(as.Date(substr(basename(tif_files[i]), 12, 19), format = "%Y%m%d"), "%Y-%m")
+  cloud_data <- data.frame(date = date, total = total_pixels, na = na_pixels, non_na = non_na_pixels, na_percent = na_percent)
+  # create plot
+  #plot(img$B8A)
+  plotRGB(img, r=3, g=2, b=1, stretch="lin", main=paste0(date, " - Cloud-Mask: ", round(na_percent, 1), "%"), axes = TRUE)
+  add_grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted", lwd = 1)
+    if (i == 1) {
+    cloud_count <- cloud_data
+  } else {
+    cloud_count <- rbind(cloud_count, cloud_data)
+  }
+  if (i == length(tif_files)) {
+    write.csv(cloud_count, "data/tables/S2_cloud_percentage_per_date.csv", row.names = FALSE)
+    cat("\rCounting finished!                                                      \n")
+    dev.off()
+  }
+}
